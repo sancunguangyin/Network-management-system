7c7e9786ff38cc00b6d50a2aeb383c7c9085abc4
sancunguangyin <59648273+sancunguangyin@users.noreply.github.com>
2020-12-15 16:22:37 +0800

    Create ScanIP.py

---
+++ ScanIP.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import commands
+import re
+
+import time
+
+
+def getAliveIP():
+    '''
+    使用nmap命令扫描局域网中活跃的IP
+    :return:
+    '''
+    status , output = commands.getstatusoutput("nmap -sP 192.168.1.0/24")
+    iplist = []
+
+    for linea in output.split("\n"):
+        if linea.find("192.168.1.") > 1:
+            ip =  re.findall(r'\d+.\d+.\d+.\d+', linea)
+            iplist.append(ip.pop())
+
+    dictIP = {}
+    dictIP['time'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    dictIP['iplist'] = iplist
+    return dictIP
