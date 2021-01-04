b2c6534db5edcbbe2e760297fd085ffb7cc1a494
sancunguangyin <59648273+sancunguangyin@users.noreply.github.com>
2021-01-04 19:17:05 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+#!/usr/bin/python
+#要执行操作的交换机管理
+swip={
+#"DEVICE_SERIAL_NUMBER" : "ip"
+'DPPMWWB76' : '172.16.200.6',
+'DPPMWWB77' : '10.57.1.22',
+};
+
+#交换机SSH用户名密码，假设有统一的管理员用户
+username = "wsf535"  #用户名
+passwd = "****"    #密码
