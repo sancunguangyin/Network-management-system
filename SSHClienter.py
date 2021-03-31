9700438bf8cc3a23cff76ce0927609be5ec0fe4d
sancunguangyin <59648273+sancunguangyin@users.noreply.github.com>
2021-03-31 15:01:24 +0800

    Update SSHClienter.py

--- SSHClienter.py
+++ SSHClienter.py
@@ -13,14 +13,18 @@ class SSHClienter():
         try:
             self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())	#自动确认陌生设备
             self.client.connect(hostname=hostname,username=username,password=password) #连接
-        except:
-            logging.critical('网络连接失败'+repr(host_ip))
-            return False
+        except NoValidConnectionsError as e:
+            return '主机%s连接失败' %(host_ip)
+        except AuthenticationException as e:
+            return '主机%s密码错误' %(host_ip)
+        except Exception as e:
+            return '未知错误:',e
+
         chan = client.invoke_shell()			#打开channel
         chan.send('screen-length 0 tem \n')		#输入命令
         time.sleep(1)
         chan.send('dis cur \n')
-        time.sleep(5)							#设置等待时间
+        time.sleep(5)					#设置等待时间
         info = chan.recv(99999).decode()		#接收输出信息
         return info
 
